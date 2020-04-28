6b428507fe9036007c6983f0f98c376a1f2a8d3d
Jaydubs <dubs@eve.vision>
2020-04-28 00:06:24 -0400

    yeet

---
+++ js/main.js
@@ -0,0 +1,15 @@
+interact('.eve-window').draggable({
+  listeners: {
+    move (event) {
+      if(!event.target.dragX || !event.target.dragY) {
+        event.target.dragX = 0;
+        event.target.dragY = 0;
+      }
+      event.target.dragX += event.dx
+      event.target.dragY += event.dy
+
+      event.target.style.transform =
+        `translate(${event.target.dragX}px, ${event.target.dragY}px)`
+    },
+  }
+})
